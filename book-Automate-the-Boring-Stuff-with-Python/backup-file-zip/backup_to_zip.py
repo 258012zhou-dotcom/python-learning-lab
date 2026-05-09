d9b00c8f14dfe3c712da4faf45ccf8cb23ba5f0c
XiaoDong <258012zhou@gmail.com>
2026-05-09 21:33:23 +0800

    backup-to-zip-2

--- book-Automate-the-Boring-Stuff-with-Python/backup-file-zip/backup_to_zip.py
+++ book-Automate-the-Boring-Stuff-with-Python/backup-file-zip/backup_to_zip.py
@@ -1,7 +1,7 @@
 import zipfile,os
 from pathlib import Path
 def backup_to_zip(folder):
-    folder=Path(folder)
+    folder=Path(folder).resolve()
 
     number=1
     while True:
@@ -11,15 +11,21 @@ def backup_to_zip(folder):
         number+=1
 
     print(f'creat {zip_filename}')
-    backup_zip=zipfile.ZipFile( zip_filename,'w')
+    # backup_zip=zipfile.ZipFile( zip_filename,'w')
+    #
+    # for folder_name, subfolders, filenames in os.walk(folder):
+    #     folder_name=Path(folder_name)
+    #     print(f'adding files in folder {folder_name}')
+    #     for filename in filenames:
+    #         print(f'adding file {filename}')
+    #         backup_zip.write(folder_name / filename)
+    # backup_zip.close()
 
-    for folder_name, subfolders, filenames in os.walk(folder):
-        folder_name=Path(folder_name)
-        print(f'adding files in folder {folder_name}')
-        for filename in filenames:
-            print(f'adding file {filename}')
-            backup_zip.write(folder_name / filename)
-    backup_zip.close()
+    with zipfile.ZipFile(zip_filename,'w',zipfile.ZIP_DEFLATED) as backup_zip:
+        for file in folder.rglob('*'):
+            if file.is_file():
+                print(f'adding file: {file.relative_to(folder)}')
+                backup_zip.write(file,arcname=file.relative_to(folder))
     print('done.')
-#backup_to_zip(Path.home() / 'spam')
+backup_to_zip(Path.home() / 'spam')
 
